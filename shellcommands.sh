bcba97425b485f3ed2ea51aa4dfa191269b93d9f
techwithnene <techwithnene@gmail.com>
2024-05-14 11:20:11 +0100

    modified

--- shellcommands.sh
+++ shellcommands.sh
@@ -1,3 +1,5 @@
 ls -la # to list contents of a directory
 pwd # to print working directory
 cat # to display the contents of a file in the terminal
+touch # to create file
+grep # to filter selection
